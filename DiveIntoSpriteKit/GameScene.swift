ebb9b76b4572b891df62f5411e5786391f316ba2
Wellesley Yu <byu@lcs.on.ca>
2020-04-16 11:20:54 -0400

    Allowed for the moving of the player by touching

--- DiveIntoSpriteKit/GameScene.swift
+++ DiveIntoSpriteKit/GameScene.swift
@@ -11,6 +11,7 @@ import SpriteKit
 @objcMembers
 class GameScene: SKScene {
     let player = SKSpriteNode(imageNamed: "player-rocket.png")
+    var touchingPlayer = false
     override func didMove(to view: SKView) {
         // this method is called when your game scene is ready to run
         let background = SKSpriteNode(imageNamed: "space.jpg")
@@ -21,18 +22,34 @@ class GameScene: SKScene {
             particles.position.x = 512
             addChild(particles)
             player.position.x = -400
+            player.zPosition = 1
             addChild(player)
          }
 }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         // this method is called when the user touches the screen
+        guard let touch = touches.first else {
+            return
+        }
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
+        if tappedNodes.contains(player) {
+            touchingPlayer = true
+        }
     }
-
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+           guard touchingPlayer else { return }
+           guard let touch = touches.first else { return }
+           let location = touch.location(in: self)
+           player.position = location
+       }
+    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         // this method is called when the user stops touching the screen
+        touchingPlayer = false
     }
-
+   
     override func update(_ currentTime: TimeInterval) {
         // this method is called before each frame is rendered
     }
